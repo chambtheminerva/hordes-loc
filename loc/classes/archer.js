9c49c13c68176c4a67a6a503b1322671b1c3add7
chambtheminerva <49500705+chambtheminerva@users.noreply.github.com>
2019-04-10 20:21:53 -0400

    Update archer.js

--- loc/classes/archer.js
+++ loc/classes/archer.js
@@ -4,13 +4,15 @@ export default {
     en: 'Archer',
     de: 'Jäger',
     se: 'Bågskytt',
-    ru: 'Лучник'
+    ru: 'Лучник',
+    fr: 'Archer'
   },
   // Class description, visible during character creation
   description: {
     en: 'Archers deal high single target damage and bursty Area of Effect damage. A good archer will always keep their distance and snipe enemies from afar.',
     de: 'Jäger richten großen Schaden bei einzelnen Zielen, und schubweise Flächenschaden an. Ein guter Jäger wird immer auf distanz bleiben.',
     se: 'Bågskytt gör hög skada mot enskilda personer och har även hög Areal skada. En bra bågskytt attackerar alltid från en distans.',
-    ru: 'Лучники наносят высокий урон по одиночной цели и взрывной урон по области. Хороший лучник всегда будет держать дистанцию и отстреливать врагов издалека.'
+    ru: 'Лучники наносят высокий урон по одиночной цели и взрывной урон по области. Хороший лучник всегда будет держать дистанцию и отстреливать врагов издалека.',
+    fr: 'Les archers infligent d\'importants dégats à une cible précise et des dégâts de zone considérables. Un bon archer garde toujours ses distances et élimine ses ennemis au loin.' 
   }
 }
